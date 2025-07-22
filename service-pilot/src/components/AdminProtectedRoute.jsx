5c70ba0ceb113a6f30b57d55db64d5f5cc46e202
Elbin12 <elbinshiby14@gmail.com>
2025-07-22 22:03:10 +0530

    removed location check in adminprotectedRoute;

--- service-pilot/src/components/AdminProtectedRoute.jsx
+++ service-pilot/src/components/AdminProtectedRoute.jsx
@@ -1,20 +1,13 @@
 import { useLocation, Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
-const ALLOWED_LOCATIONS = ["b8qvo7VooP3JD3dIZU42", "dIzpiRxQkIWkmFfr8T5j"];
-
 const AdminProtectedRoute = ({ children }) => {
-  const location = useLocation();
-  const params = new URLSearchParams(location.search);
-  const locationId = params.get("location");
-
   const { isLoginned, admin_info } = useSelector(state => state.admin);
   const isLoggedIn = isLoginned || !!admin_info;
 
   // Allow if admin is logged in or if a valid locationId is passed
-  const isAllowedViaLocation = locationId && ALLOWED_LOCATIONS.includes(locationId);
 
-  if (isLoggedIn || isAllowedViaLocation) {
+  if (isLoggedIn) {
     return children;
   }
 
